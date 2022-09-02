7727a06e70739f0bf042c089918096f344234c78
limeKIWI <pop78978@gmail.com>
2022-09-03 00:05:04 +0900

    #2 댓글들 업데이트 메소드와 작성자 확인 메소드 추가

--- src/main/java/com/example/week6project/domain/comments/DiceComment.java
+++ src/main/java/com/example/week6project/domain/comments/DiceComment.java
@@ -1,6 +1,8 @@
 package com.example.week6project.domain.comments;
 
+import com.example.week6project.controller.request.CommentRequestDto;
 import com.example.week6project.domain.Member;
+import com.example.week6project.domain.Timestamped;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,7 +13,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Builder
-public class DiceComment {
+public class DiceComment extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,5 +25,13 @@ public class DiceComment {
 
     @Column(nullable = false)
     private String content;
+
+    public void update(CommentRequestDto requestDto) {
+        this.content = requestDto.getContent();
+    }
+
+    public boolean validateMember(Member member) {
+        return !this.member.equals(member);
+    }
 
 }
