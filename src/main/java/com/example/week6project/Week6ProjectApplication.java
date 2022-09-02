912dc3dc36adbaa5762557fb37b9529796b94862
limeKIWI <pop78978@gmail.com>
2022-09-02 15:48:21 +0900

    기본 레포지토리 생성

---
+++ src/main/java/com/example/week6project/Week6ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.week6project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6ProjectApplication.class, args);
+    }
+
+}
