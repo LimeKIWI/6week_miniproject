226f76a118d4b52c8f1bb68d3aac81992716f4af
limeKIWI <pop78978@gmail.com>
2022-09-02 21:19:43 +0900

    #2 Comment Dto 생성

---
+++ src/main/java/com/example/week6project/controller/response/CommentResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.week6project.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDto {
+    private Long id;
+    private String nickName;
+    private String content;
+}
