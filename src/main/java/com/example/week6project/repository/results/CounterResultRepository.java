0f362fcb13a6711add0f5c4929dcc06ba29cddd6
limeKIWI <pop78978@gmail.com>
2022-09-03 19:58:24 +0900

    #3 게임 결과테이블에서 null이 반환될 일이 없으므로 객체 타입으로 반환하게 변경

--- src/main/java/com/example/week6project/repository/results/CounterResultRepository.java
+++ src/main/java/com/example/week6project/repository/results/CounterResultRepository.java
@@ -1,7 +1,10 @@
 package com.example.week6project.repository.results;
 
+import com.example.week6project.domain.Member;
 import com.example.week6project.domain.results.CounterResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CounterResultRepository extends JpaRepository<CounterResult, Long> {
+    CounterResult findByMember(Member member);
 }
--- src/main/java/com/example/week6project/repository/results/DiceResultRepository.java
+++ src/main/java/com/example/week6project/repository/results/DiceResultRepository.java
@@ -4,8 +4,7 @@ import com.example.week6project.domain.Member;
 import com.example.week6project.domain.results.DiceResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface DiceResultRepository extends JpaRepository<DiceResult, Long> {
-    Optional<DiceResult> findByMember(Member member);
+    DiceResult findByMember(Member member);
 }
