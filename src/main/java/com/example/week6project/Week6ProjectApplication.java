29c5a0263979b76f95d00f39501930a1ce789616
limeKIWI <pop78978@gmail.com>
2022-09-05 22:50:11 +0900

    #12 CORS 수정

--- src/main/java/com/example/week6project/Week6ProjectApplication.java
+++ src/main/java/com/example/week6project/Week6ProjectApplication.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableScheduling
 @SpringBootApplication
 public class Week6ProjectApplication {
-
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+    public static final String ALLOWED_HEADERS = "Origin, Content-Type, X-Auth-Token";
     public static void main(String[] args) {
         SpringApplication.run(Week6ProjectApplication.class, args);
     }
@@ -22,6 +23,8 @@ public class Week6ProjectApplication {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                        .allowedHeaders(ALLOWED_HEADERS.split(","))
                         .exposedHeaders("Authorization", "RefreshToken")
                         .maxAge(3600);
             }
