b0d1dede0e0c360476699532d2fc8e2ac45e5d67
limeKIWI <pop78978@gmail.com>
2022-09-06 15:18:46 +0900

    #6 멤버가 작성한 코멘트 찾기 작성

--- src/main/java/com/example/week6project/repository/comments/CommentRepository.java
+++ src/main/java/com/example/week6project/repository/comments/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.example.week6project.repository.comments;
 
 import com.example.week6project.domain.GameType;
+import com.example.week6project.domain.Member;
 import com.example.week6project.domain.comments.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByGameTypeEnumOrderByModifiedAtDesc(Enum<GameType> gameTypeEnum);
+    List<Comment> findAllByMember(Member member);
 }
