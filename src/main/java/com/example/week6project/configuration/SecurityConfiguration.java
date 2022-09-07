d372311c8c09ffef77579bebbff0d933066d3542
limeKIWI <pop78978@gmail.com>
2022-09-07 16:26:20 +0900

    #12 CORS정책에 프론트출처 추가

--- src/main/java/com/example/week6project/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/week6project/configuration/SecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
